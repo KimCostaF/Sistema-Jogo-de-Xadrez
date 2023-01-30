8df0dd3be18b94792631507587c124f97b574046
Kim Costa Ferro Alves <kcostaferro@gmail.com>
2023-01-30 12:18:27 -0300

    Começando a Implementar Classes "Peca e Tabuleiro"

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
-import Tabuleiro.Posicao;
+import TabuleiroJogo.Posicao;
+import TabuleiroJogo.Tabuleiro;
 
 public class Main {
     public static void main(String[] args) {
         Posicao pos = new Posicao(3,5);
         System.out.println(pos);
+
+        Tabuleiro tabuleiro = new Tabuleiro(8,3);
+
     }
 }
