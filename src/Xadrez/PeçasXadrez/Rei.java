da34662a3fb331ff4535e86454e54cc5c21ce393
Kim Costa Ferro Alves <kcostaferro@gmail.com>
2023-02-03 11:43:34 -0300

    Implementando movimentos do rei

--- src/Xadrez/PeçasXadrez/Rei.java
+++ src/Xadrez/PeçasXadrez/Rei.java
@@ -1,5 +1,7 @@
 package Xadrez.PeçasXadrez;
 
+import TabuleiroJogo.Peca;
+import TabuleiroJogo.Posicao;
 import TabuleiroJogo.Tabuleiro;
 import Xadrez.Cor;
 import Xadrez.PecaXadrez;
@@ -14,9 +16,70 @@ public class Rei extends PecaXadrez {
         return "R";
     }
 
+    private boolean podeMover(Posicao posicao){
+        PecaXadrez p = (PecaXadrez)getTabuleiro().peca(posicao);
+        return p==null || p.getCor() != getCor();
+    }
+
     @Override
     public boolean[][] acoesPossiveis() {
         boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao p = new Posicao(0,0);
+
+        //Movimento vertical cima
+        p.setValores(posicao.getLinha()-1,posicao.getColuna());
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento vertical para baixo
+        p.setValores(posicao.getLinha()+1,posicao.getColuna());
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento horizontal para esquerda
+        p.setValores(posicao.getLinha(),posicao.getColuna()-1);
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento horizontal para direita
+        p.setValores(posicao.getLinha(),posicao.getColuna()+1);
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento diagonal superior esquerdo
+        p.setValores(posicao.getLinha()-1,posicao.getColuna()-1);
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento diagonal superior direito
+        p.setValores(posicao.getLinha()-1,posicao.getColuna()+1);
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento diagonal inferior esquerdo
+        p.setValores(posicao.getLinha()+1,posicao.getColuna()-1);
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //Movimento diagonal inferior direito
+        p.setValores(posicao.getLinha()+1,posicao.getColuna()+1);
+        if (getTabuleiro().posicaoExiste(p)&&podeMover(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+
+
+
         return mat;
     }
+
+
 }
